Create separate modules, restructure, fix average of demos

environment.py
@@ -1,234 +1,145 @@
-import abc
-
 import numpy as np
 
+from storage import ObjectStorage
 
-class Object:
+
+class Environment:
     """
-    The basic class for objects in the environment. Each object has a latent representation, a visible representation,
-    and a task type.
+    A class to represent the environment. It provides an interface to the solver which abstracts the storage, oracle,
+    and objects.
     """
 
-    def __init__(self, index, latent_representation, task_type, c):
+    def __init__(self, c, oracle):
         """
-        Initialise the object
-        :param index: The index of the object
-        :param latent_representation: The latent representation of the object
-        :param task_type: The task type of the object
+        Initialise the environment
+        :param c: The configuration
+        :param oracle: The oracle to use
         """
-        assert isinstance(latent_representation, np.ndarray), f"Expected np.ndarray, got {type(latent_representation)}"
-        assert len(latent_representation.shape) == 1, f"Expected 1D array, got {len(latent_representation.shape)}D"
-        assert latent_representation.shape[
-                   0] == c.LATENT_DIM, f"Expected array of length {c.LATENT_DIM}, got {latent_representation.shape[0]}"
-
         self.c = c
-        self.name = f"Object {index}"
-        self.index = index
-        self._latent_repr = latent_representation
-        self.visible_repr = self.create_visible_representation()
-        self.task_type = task_type
-        self.demo = None
+        self.storage = ObjectStorage(c, oracle, self)
 
-    def create_visible_representation(self):
+    def try_trajectory(self, trajectory, waypoints):
         """
-        Create a visible representation of the object. This is done by adding some noise to the latent representation
-        :return: The visible representation of the object
-        """
-        return self._latent_repr + np.random.normal(0, self.c.VISIBLE_REPRESENTATION_NOISE,
-                                                    self._latent_repr.shape)
-
-    def get_visual_similarity(self, other):
-        """
-        Get the similarity between the visible representations of this object and another object
-        :param other: The other object
-        :return: The similarity between the visible representations. Implemented as the cosine similarity
-        """
-        return np.dot(self.visible_repr, other.visible_repr) / (
-                np.linalg.norm(self.visible_repr) * np.linalg.norm(other.visible_repr))
-
-    def get_latent_similarity(self, other):
-        """
-        Get the similarity between the latent representations of this object and another object
-        :param other: The other object
-        :return: The similarity between the latent representations. Implemented as the dot product
-        """
-        return np.dot(self._latent_repr, other._latent_repr)
-
-    def get_task_type_correspondence(self, other):
-        """
-        Get the task type correspondence between this object and another object
-        :param other: The other object
-        :return: True if the task types are the same and False otherwise
-        """
-        return self.task_type == other.task_type
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        return f"{self.name} ({self._latent_repr}), {self.visible_repr}, {self.task_type}, {self.demo}"
-
-    def try_action(self, action):
-        """
-        A super simple action which is just trying to predict the "final position" of the object which is some function
-        of the latent representation and task type
-        """
-        return self.check_position(action)
-
-    def check_position(self, position):
-        """
-        Check if the position is close to the "position" of the object
-        :param position: The position to check
-        :return: True if the position is close to the "position" of the object
-        """
-        return np.allclose(position, self.get_position(), atol=self.c.POSITION_TOLERANCE)
-
-    def get_position(self):
-        """
-        Returns the "position" of the object which is some function of the latent representation and task type.
-        For now, it is just the latent representation multiplied by the task type
-        :return: The "position" of the object
-        """
-        return self._latent_repr
-
-    def get_demo(self):
-        return self.get_position()
-
-
-class TrajectoryObject(Object):
-    """
-    An object that can be interacted with by providing a trajectory of actions
-
-    This assumes the frame is object-centric so there is no need to provide the start pose
-    """
-
-    def __init__(self, index, latent_representation, task_type, c):
-        """
-        Initialise the object
-
-        :param index: The object index
-        :param latent_representation: The latent representation of the object
-        :param task_type: The task type of the object
-        """
-        super().__init__(index, latent_representation, task_type, c)
-        self.waypoints = []
-
-    def try_trajectory(self, trajectory):
-        """
-        Try a trajectory of actions. A trajectory is a list of actions that are applied to the object, and it is
-        considered successful if the object goes through all waypoints
+        Try a trajectory to see if it reaches the waypoints
         :param trajectory: The trajectory to try
-        :return: True if the trajectory is successful and False otherwise
+        :param waypoints: The waypoints to reach
+        :return: True if the trajectory reaches the waypoints and False otherwise
         """
-        current_position = np.zeros_like(self.get_position())
+        current_position = np.zeros_like(waypoints[0])
         waypoint_index = 0
         for action in trajectory:
             current_position += action
-            if np.allclose(current_position, self.waypoints[waypoint_index], atol=self.c.POSITION_TOLERANCE):
+            if np.allclose(current_position, waypoints[waypoint_index], atol=self.c.POSITION_TOLERANCE):
                 waypoint_index += 1
-                if waypoint_index == len(self.waypoints):
+                if waypoint_index == len(waypoints):
                     break
-        return waypoint_index == len(self.waypoints)
+        return waypoint_index == len(waypoints)
 
-    def get_demo(self):
-        actions = generate_trajectory(self.waypoints, self.c)
+    def generate_trajectory(self, waypoints):
+        """
+        Generate a trajectory that goes through the waypoints
+        :param waypoints: The waypoints to go through
+        :return: The trajectory
+        """
+        trajectory = []
+        trajectory.extend(self.generate_trajectory_between(np.zeros_like(waypoints[0]), waypoints[0]))
+        for i in range(len(waypoints) - 1):
+            trajectory.extend(self.generate_trajectory_between(waypoints[i], waypoints[i + 1]))
+        trajectory = np.array(trajectory)
+        assert self.try_trajectory(trajectory, waypoints)
+        return trajectory
+
+    def generate_trajectory_between(self, start_pos, end_pos):
+        """
+        Generate a trajectory between two points
+        :param start_pos: The start position
+        :param end_pos: The end position
+        :return: The trajectory
+        """
+        num_steps = max(self.c.MIN_TRAJ_STEPS,
+                        np.ceil(np.linalg.norm(end_pos - start_pos) / self.c.MAX_ACTION * 2).astype(int))
+        traj = np.linspace(start_pos, end_pos, num_steps)
+        actions = np.diff(traj, axis=0)
+        for i in range(actions.shape[0] - 1):
+            noise = np.random.normal(0, self.c.ACTION_EXPLORATION_DEVIATION, actions[i].shape)
+            actions[i] += noise
+            actions[i + 1] -= noise
+        assert np.allclose(np.sum(actions, axis=0) + start_pos, end_pos, atol=self.c.POSITION_TOLERANCE)
         return actions
 
-    def generate_waypoints(self):
+    def select_object_to_try(self):
         """
-        Generate waypoints the object needs to pass through
+        Select an object to try
+        :return: The index of the object to try
         """
-        above = np.zeros_like(self.get_position())
-        above[-1] = 0.1
-        next_to = np.zeros_like(self.get_position())
-        next_to[-1] = 0.2
-        if self.task_type == "gripping":
-            # for gripping, pick a waypoint above the object, then the object, and back above the object
-            self.waypoints = [self.get_position() + above, self.get_position(), self.get_position() + above]
-        elif self.task_type == "pushing":
-            # for pushing, pick a waypoint at the object position, then a waypoint further away
-            self.waypoints = [self.get_position(), self.get_position() + next_to]
-        elif self.task_type == "inserting":
-            # for inserting, pick a waypoint above the object, then at the object position
-            self.waypoints = [self.get_position() + above, self.get_position()]
-        self.waypoints = np.array(self.waypoints)
+        return self.storage.select_object_to_try()
 
+    def update_object(self, obj, success, trajectory):
+        """
+        Update the object with the success of the trajectory
+        :param obj: The object to update
+        :param success: Whether the trajectory was successful
+        :param trajectory: The trajectory that was tried
+        """
+        self.storage.update_object(obj, success, trajectory)
 
-def try_trajectory(trajectory, waypoints, c):
-    """
-    Try a trajectory of actions. A trajectory is a list of actions that are applied to the object, and it is
-    considered successful if the object goes through all waypoints
-    :param trajectory: The trajectory to try
-    :return: True if the trajectory is successful and False otherwise
-    """
-    current_position = np.zeros_like(waypoints[0])
-    waypoint_index = 0
-    for action in trajectory:
-        current_position += action
-        if np.allclose(current_position, waypoints[waypoint_index], atol=c.POSITION_TOLERANCE):
-            waypoint_index += 1
-            if waypoint_index == len(waypoints):
-                break
-    return waypoint_index == len(waypoints)
+    def has_unknown_objects(self):
+        """
+        Check if there are unknown objects
+        :return: True if there are unknown objects and False otherwise
+        """
+        return self.storage.has_unknown_objects()
 
+    def find_most_similar_known_object_demo(self, u):
+        """
+        Find the most similar known object to the unknown object
+        :param u: The unknown object index
+        :return: The demonstration of the most similar known object
+        """
+        return self.storage.find_most_similar_known_object_demo(u)
 
-def generate_trajectory(waypoints, c):
-    """
-    Generate a trajectory that goes through all waypoints
-    :param waypoints: The waypoints to go through
-    :return: The trajectory that goes through all waypoints
-    """
-    trajectory = []
-    trajectory.extend(generate_trajectory_between(np.zeros_like(waypoints[0]), waypoints[0], c))
-    for i in range(len(waypoints) - 1):
-        trajectory.extend(generate_trajectory_between(waypoints[i], waypoints[i + 1], c))
-    trajectory = np.array(trajectory)
-    assert try_trajectory(trajectory, waypoints, c)
-    return trajectory
+    def find_top_k_most_similar_known_objects_demo(self, u):
+        """
+        Find the top k most similar known objects to the unknown object
+        :param u: The unknown object index
+        :return: The demonstrations of the top k most similar known objects
+        """
+        return self.storage.find_top_k_most_similar_known_objects_demo(u)
 
+    def get_objects(self):
+        """
+        Get all the objects in the environment
+        :return: The objects
+        """
+        return self.storage.objects
 
-def generate_trajectory_between(start_pos, end_pos, c):
-    num_steps = max(5, np.ceil(np.linalg.norm(end_pos - start_pos) / c.MAX_ACTION * 2).astype(int))
-    traj = np.linspace(start_pos, end_pos, num_steps)
-    actions = np.diff(traj, axis=0)
-    for i in range(actions.shape[0] - 1):
-        noise = np.random.normal(0, c.ACTION_EXPLORATION_DEVIATION, actions[i].shape)
-        actions[i] += noise
-        actions[i + 1] -= noise
-    assert np.allclose(np.sum(actions, axis=0) + start_pos, end_pos, atol=c.POSITION_TOLERANCE)
-    return actions
+    def get_position(self, obj_ind):
+        """
+        Get the position of an object
+        :param obj_ind: The index of the object
+        :return: The position of the object
+        """
+        return self.storage.objects[obj_ind].get_position()
 
+    def try_object_trajectory(self, obj_ind, trajectory):
+        """
+        Try a trajectory for an object
+        :param obj_ind: The index of the object
+        :param trajectory: The trajectory to try
+        """
+        return self.try_trajectory(trajectory, self.storage.objects[obj_ind].waypoints)
 
-class Oracle(abc.ABC):
-    """
-    An abstract class for an oracle that can provide demonstrations
+    def get_trajectory_end(self, trajectory):
+        """
+        Get the end position of a trajectory
+        :param trajectory: The trajectory
+        :return: The end position
+        """
+        return np.sum(trajectory, axis=0)
 
-    The oracle has a cost associated with using it and can provide a demonstration for an object
-    """
-
-    def __init__(self, cost):
-        self.cost_per = cost
-        self.total_cost = 0
-
-    def get_final_cost(self):
-        return self.total_cost
-
-    def reset_cost(self):
-        self.total_cost = 0
-
-    @abc.abstractmethod
-    def get_demo(self, obj, c):
-        pass
-
-
-class NoisyOracle(Oracle):
-    def get_demo(self, obj, c):
-        self.total_cost += self.cost_per
-        obj_demo = obj.get_demo()
-        return obj_demo + np.random.normal(0, c.DEMO_NOISE, obj_demo.shape)
-
-
-class PerfectOracle(Oracle):
-    def get_demo(self, obj, c):
-        self.total_cost += self.cost_per
-        return obj.get_demo()
+    def get_selection_frequency(self):
+        """
+        Get the selection frequency of the known objects
+        :return: The selection frequency of the known objects
+        """
+        return self.storage.selection_frequency

optim.py
@@ -71,7 +71,8 @@ def reorder(unlabelled_object_indices, labelled_object_indices, distances, metho
             dist_sum[i] = np.sum(distances[unlabelled_object_indices][:c])
         min_ind = np.argmin(dist_sum)
 
-        return np.concatenate(([closest_indices[min_ind]], unlabelled_object_indices[unlabelled_object_indices != closest_indices[min_ind]]))
+        return np.concatenate(([closest_indices[min_ind]],
+                               unlabelled_object_indices[unlabelled_object_indices != closest_indices[min_ind]]))
     elif method == "beam_search":
         k = 25
         sequences = []

runner.py
@@ -1,16 +1,17 @@
 import numpy as np
 
 from solver import run_experiment
+from config import Config
 
 if __name__ == "__main__":
     # TODO Add a cli to specify the configuration
-
+    c = Config()
     seeds_to_try = np.arange(10)
 
     all_results = []
     for seed in seeds_to_try:
         print("Running experiment with seed", seed)
-        result = run_experiment(seed)
+        result = run_experiment(seed, c)
         all_results.append(result)
 
     print("Average total cost", np.mean([r["total_cost"] for r in all_results]))

solver.py
@@ -1,197 +1,101 @@
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cluster import KMeans
 
 from config import Config
-from environment import NoisyOracle, TrajectoryObject
+from environment import Environment
+from oracle import NoisyOracle
+from visualiser import Visualiser
 
 
-def try_object(obj, base_trajectory, c):
+def try_object(env, obj_ind, base_trajectory, c):
     """
-    Try to solve the object by applying the base action with some noise
-    :param obj: The object to solve
-    :param base_trajectory: The base trajectory to apply
-    :return: The number of tries it took to solve the object and whether the object was solved
+    Try to solve the object with the given trajectory
+    :param env: The environment
+    :param obj_ind: The object index
+    :param base_trajectory: The base trajectory to try
+    :param c: The configuration
     """
     counter = 0
     trajectory = base_trajectory
-    failed = not obj.try_trajectory(trajectory)
+    failed = not env.try_object_trajectory(obj_ind, trajectory)
     while failed:
         trajectory = base_trajectory + np.random.normal(0, c.ACTION_EXPLORATION_DEVIATION, base_trajectory.shape)
-        failed = not obj.try_trajectory(trajectory)
+        failed = not env.try_object_trajectory(obj_ind, trajectory)
         counter += 1
         if counter > c.EXPLORATION_TRIES:
             if c.VERBOSITY > 0:
-                print(f"Failed to find {obj.get_position()} with trajectory ending {np.sum(base_trajectory, axis=0)} "
-                      f"for object {obj}")
+                print(f"Failed to find {env.get_position(obj_ind)} with trajectory ending "
+                      f"{env.get_trajectory_end(base_trajectory)} for object {obj_ind}")
             break
     return counter, not failed, trajectory
 
 
-def select_objects_to_try(visual_similarities, known_objects, unknown_objects, object_types, objects, c):
-    task_types = np.unique(object_types[known_objects])
-    candidates = []
-    for t_t in task_types:
-        # find the known and unknown objects that have the task type t_t
-        known_objects_t = known_objects[object_types[known_objects] == t_t]
-        # filter out known objects where the demo is None
-        known_objects_t = known_objects_t[[objects[i].demo is not None for i in known_objects_t]]
-        unknown_objects_t = unknown_objects[object_types[unknown_objects] == t_t]
-        if len(known_objects_t) == 0 or len(unknown_objects_t) == 0:
-            continue
-        # find the object from the unknown objects that is most similar to one of the known objects
-        u = unknown_objects_t[np.argsort(np.max(visual_similarities[unknown_objects_t][:, known_objects_t], axis=1))][
-            -1]
-        # find the corresponding known object taking into account the task type
-        k = known_objects_t[np.argmax(visual_similarities[u, known_objects_t])]
-        candidates.append((u, k))
-        if c.VERBOSITY > 1:
-            print(f"Task type {t_t}, unknown object {u}, known object {k}, similarity {visual_similarities[u, k]}")
-
-    # find the pair of objects that are most similar
-    u, k = candidates[np.argmax([visual_similarities[u, k] for u, k in candidates])]
-    if c.VERBOSITY > 1:
-        print(f"Selected unknown object {u}, known object {k}, similarity {visual_similarities[u, k]}")
-    return u, k
-
-
-def create_figure(objects, field, c):
-    to_plot = {t: [] for t in c.TASK_TYPES}
-    for i, o in enumerate(objects):
-        to_plot[o.task_type].append(getattr(o, field))
-    for t in c.TASK_TYPES:
-        to_plot[t] = np.array(to_plot[t])
-        plt.scatter(to_plot[t][:, 0], to_plot[t][:, 1], label=t)
-
-
-def generate_objects(c):
-    objects = []
-    for i in range(c.OBJ_NUM):
-        objects.append(TrajectoryObject(i, np.random.uniform(0, 1, c.LATENT_DIM), -1, c))
-    # cluster the objects into different task types based on the latent representation
-    k = KMeans(n_clusters=len(c.TASK_TYPES)).fit(np.array([o._latent_repr for o in objects]))
-    for i, o in enumerate(objects):
-        o.task_type = c.TASK_TYPES[k.labels_[i]]
-        o.generate_waypoints()
-
-    create_figure(objects, "_latent_repr", c)
-    plt.savefig("objects_latent.png")
-    plt.clf()
-    create_figure(objects, "visible_repr", c)
-    plt.savefig("objects_visible.png")
-    plt.clf()
-
-    return np.array(objects, dtype=TrajectoryObject)
-
-
-def generate_helper_data(objects, oracle, c):
-    # find random objects to be provided as known but make sure all task types are represented
-    known_objects = np.random.choice(c.OBJ_NUM, c.KNOWN_OBJECT_NUM, replace=False)
-    while len(set([objects[i].task_type for i in known_objects])) < len(c.TASK_TYPES):
-        if c.VERBOSITY > 0:
-            print("Retrying object selection to ensure all task types are represented")
-        known_objects = np.random.choice(c.OBJ_NUM, c.KNOWN_OBJECT_NUM, replace=False)
-
-    for i in known_objects:
-        objects[i].demo = oracle.get_demo(objects[i], c)
-    oracle.reset_cost()
-
-    unknown_objects = np.array([i for i in range(c.OBJ_NUM) if i not in known_objects])
-
-    visual_similarities = np.zeros((c.OBJ_NUM, c.OBJ_NUM))
-    for i in range(c.OBJ_NUM):
-        for j in range(c.OBJ_NUM):
-            visual_similarities[i, j] = objects[i].get_visual_similarity(objects[j])
-
-    # apply min-max normalization to the visual similarities
-    min_ = -1
-    max_ = 1
-    visual_similarities = (visual_similarities - min_) / (max_ - min_)
-
-    object_types = [objects[i].task_type for i in range(c.OBJ_NUM)]
-    object_types = np.array(object_types)
-
-    return known_objects, unknown_objects, visual_similarities, object_types
-
-
-def solve_objects(objects, known_objects, unknown_objects, visual_similarities, object_types, c):
+def solve_objects(env, c):
+    """
+    Solve all objects in a given environment
+    :param env: The environment
+    :param c: The configuration
+    """
     all_tries = []
     failed_objects = []
-    selection_frequency = np.zeros(c.OBJ_NUM)
-    while len(known_objects) < c.OBJ_NUM:
-        u, k = select_objects_to_try(visual_similarities, known_objects, unknown_objects, object_types, objects, c)
-        selection_frequency[k] += 1
-        # if the objects are quite similar, try replaying the known demo
-        if c.VERBOSITY > 0 and not objects[u].get_task_type_correspondence(objects[k]):
-            print(f"Similarity {visual_similarities[u, k]}, matching tasks "
-                  f"{objects[u].get_task_type_correspondence(objects[k])}")
-        if c.VERBOSITY > 1:
-            print(f"Replaying demo for object {objects[k]} to solve object {objects[u]} with similarity "
-                  f"{visual_similarities[u, k]}")
+    while env.has_unknown_objects():
+        u = env.select_object_to_try()
 
         if c.METHOD == "single":
-            demo = objects[k].demo
+            demo = env.find_most_similar_known_object_demo(u)
         elif c.METHOD == "average":
-            top_k = known_objects[np.argsort((visual_similarities[u][known_objects]))]
-            # find the last k that have a demo not None
-            top_k = top_k[[objects[i].demo is not None for i in top_k]][-c.TOP_K:]
-            weights = visual_similarities[u, top_k]
-            # since demonstrations can have different weights, we need to average what we can
-            max_demo_len = max([len(objects[i].demo) for i in top_k])
-            demo = np.zeros((max_demo_len, c.LATENT_DIM))
-            demo_weights = np.zeros((max_demo_len, c.LATENT_DIM))
-            for i in range(c.TOP_K):
-                for j in range(max_demo_len):
-                    if j < len(objects[top_k[i]].demo):
-                        demo[j] += objects[top_k[i]].demo[j]
-                        demo_weights[j] += weights[i] * np.ones(c.LATENT_DIM)
-            demo = demo / demo_weights
+            demo = env.find_top_k_most_similar_known_objects_demo(u)
         else:
             raise ValueError(f"Unknown method {c.METHOD}")
 
-        tries, success, trajectory = try_object(objects[u], demo, c)
+        tries, success, trajectory = try_object(env, u, demo, c)
 
         if not success:
             failed_objects.append(u)
         else:
-            objects[u].demo = trajectory
             all_tries.append(tries)
 
-        known_objects = np.append(known_objects, u)
-        unknown_objects = np.array([i for i in range(c.OBJ_NUM) if i not in known_objects])
+        env.update_object(u, success, trajectory)
+
     if c.VERBOSITY > 0:
-        print(f"Selection frequency: {selection_frequency}")
+        print(f"Selection frequency: {env.get_selection_frequency()}")
     return all_tries, failed_objects
 
 
 def run_experiment(seed, c):
+    """
+    Run an experiment with a given seed and configuration
+    :param seed: The seed
+    :param c: The configuration
+    """
     start_time = time.time()
     np.random.seed(seed)
     oracle = NoisyOracle(1)
 
-    objects = generate_objects(c)
-    known_objects, unknown_objects, visual_similarities, object_types = generate_helper_data(objects, oracle, c)
-
-    all_tries, failed_objects = solve_objects(objects, known_objects, unknown_objects, visual_similarities,
-                                              object_types, c)
-
-    total_cost = oracle.get_final_cost()
-    exploration_tries = all_tries
-    exploration_tries_mean = np.mean(exploration_tries) if len(exploration_tries) > 0 else 0
-    failures_from_exploration_count = len([o for o in failed_objects])
+    environment = Environment(c, oracle)
+    visualiser = Visualiser(c)
 
     if c.VERBOSITY > 0:
-        create_figure(objects, "_latent_repr")
-        for obj_ind in failed_objects:
-            plt.scatter(objects[obj_ind]._latent_repr[0], objects[obj_ind]._latent_repr[1], color="red")
-        plt.savefig("objects_with_failures.png")
-        plt.clf()
-        create_figure(objects, "visible_repr")
-        for obj_ind in failed_objects:
-            plt.scatter(objects[obj_ind].visible_repr[0], objects[obj_ind].visible_repr[1], color="red")
-        plt.savefig("objects_visible_with_failures.png")
+        visualiser.create_figure_for_all(environment.get_objects(), "_latent_repr")
+        visualiser.save_figure("objects_latent.png")
+        visualiser.create_figure_for_all(environment.get_objects(), "visible_repr")
+        visualiser.save_figure("objects_visible.png")
+
+    all_tries, failed_object_indices = solve_objects(environment, c)
+
+    exploration_tries = all_tries
+    exploration_tries_mean = np.mean(exploration_tries) if len(exploration_tries) > 0 else 0
+    failures_from_exploration_count = len(failed_object_indices)
+
+    if c.VERBOSITY > 0:
+        visualiser.create_figure_for_all(environment.get_objects(), "_latent_repr")
+        visualiser.create_figure_for_ind(environment.get_objects(), failed_object_indices, "_latent_repr", "red")
+        visualiser.save_figure("objects_latent_with_failures.png")
+
+        visualiser.create_figure_for_all(environment.get_objects(), "visible_repr")
+        visualiser.create_figure_for_ind(environment.get_objects(), failed_object_indices, "visible_repr", "red")
+        visualiser.save_figure("objects_visible_with_failures.png")
 
         print(f"Average tries with exploration: {np.mean(exploration_tries)}")
         print(f"Standard deviation of tries with exploration: {np.std(exploration_tries)}")
@@ -201,7 +105,6 @@ def run_experiment(seed, c):
 
     return {
         "seed": seed,
-        "total_cost": total_cost,
         "exploration_tries": exploration_tries_mean,
         "failures_from_exploration_count": failures_from_exploration_count,
         "time": time.time() - start_time
@@ -209,13 +112,13 @@ def run_experiment(seed, c):
 
 
 if __name__ == "__main__":
-    # TODO Rework the storage system to distinguish between different tasks
+    # TODO something clever in storage.update_objects - potentially keep the object as unknown but wait to try it again
     c = Config()
     c.METHOD = "single"
-    run_experiment(0, c)
+    res = run_experiment(0, c)
     c.METHOD = "average"
     c.TOP_K = 1
-    run_experiment(0, c)
-    # for k in range(1, 10):
-    #     c.TOP_K = k
-    #     run_experiment(0, c)
+    res2 = run_experiment(0, c)
+
+    assert res["exploration_tries"] == res2["exploration_tries"]
+    assert res["failures_from_exploration_count"] == res2["failures_from_exploration_count"]