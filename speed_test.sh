a8d0666735b6a09c5858c8bf5686df3e9db4de19
Mikolaj Deja <dejamikolaj@gmail.com>
2024-06-15 18:04:36 +0100

    Add cpu speed test

---
+++ speed_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=md1823
+#SBATCH --output=out_sa_hyper_%j.out
+
+
+source /vol/bitbucket/md1823/taskmaster/learning/venv/bin/activate
+
+python3 -m speed_test
