f7cbf6a3a97228e830b92bad141e70eecd22a5cd
Mikolaj Deja <dejamikolaj@gmail.com>
2024-08-13 11:56:11 +0100

    Remove resize

--- helper.py
+++ helper.py
@@ -105,7 +105,6 @@ def get_linearly_probed(a, b, config, ps):
 def get_nn_sim(a, b, model_config, model_path, config):
     transform = transforms.Compose(
         [
-            transforms.Resize((400, 400)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
