53dd6b6339b6a84befeab20c90830d18fbc4c4f0
Mikolaj Deja <dejamikolaj@gmail.com>
2024-08-13 13:24:03 +0100

    Add siamese embeddings to sim eval and approx calc

--- analysis/approximation_calculations.py
+++ analysis/approximation_calculations.py
@@ -8,7 +8,8 @@ from sklearn.metrics import f1_score
 from analysis.similarity_measure_eval import extract_features
 from config import Config
 from playground.environment import Environment
-from tm_utils import ImagePreprocessing, ImageEmbeddings, SimilarityMeasure
+from tm_utils import ImagePreprocessing, ImageEmbeddings, SimilarityMeasure, ContourImageEmbeddings, \
+    ContourSimilarityMeasure, NNImageEmbeddings, NNSimilarityMeasure
 
 
 def convert_binary(arr, threshold):
@@ -44,6 +45,22 @@ def get_affine_fn(ls, vs):
     return float(slope), float(intercept)
 
 
+def run_one(config):
+    start = time.time()
+    environment = Environment(config)
+    ls_all, vs_all, tasks = extract_features(environment, config)
+    thresholds = []
+    affine_fns = []
+    for ls, vs, task in zip(ls_all, vs_all, tasks):
+        threshold = get_threshold(ls, vs, config)
+        affine_fn = get_affine_fn(ls, vs)
+        thresholds.append(threshold)
+        affine_fns.append(affine_fn)
+
+    print(f"Finished {str(config)} in {time.time() - start}")
+    return thresholds, affine_fns
+
+
 if __name__ == "__main__":
     config = Config()
     config.OBJ_NUM = 51
@@ -67,19 +84,23 @@ if __name__ == "__main__":
         for emb in ImageEmbeddings:
             config.IMAGE_EMBEDDINGS = emb
             for sim in SimilarityMeasure:
-                start = time.time()
                 config.SIMILARITY_MEASURE = sim
-                environment = Environment(config)
-                ls_all, vs_all, tasks = extract_features(environment, config)
-                thresholds = []
-                affine_fns = []
-                for ls, vs, task in zip(ls_all, vs_all, tasks):
-                    threshold = get_threshold(ls, vs, config)
-                    affine_fn = get_affine_fn(ls, vs)
-                    thresholds.append(threshold)
-                    affine_fns.append(affine_fn)
+                thresholds, affine_fns = run_one(config)
                 data[str(config)] = {"thresholds": thresholds, "affine_fns": affine_fns}
-                print(f"Finished {str(config)} in {time.time() - start}")
+
+        for emb in ContourImageEmbeddings:
+            config.IMAGE_EMBEDDINGS = emb
+            for sim in ContourSimilarityMeasure:
+                config.SIMILARITY_MEASURE = sim
+                thresholds, affine_fns = run_one(config)
+                data[str(config)] = {"thresholds": thresholds, "affine_fns": affine_fns}
+
+        for emb in NNImageEmbeddings:
+            config.IMAGE_EMBEDDINGS = emb
+            for sim in NNSimilarityMeasure:
+                config.SIMILARITY_MEASURE = sim
+                thresholds, affine_fns = run_one(config)
+                data[str(config)] = {"thresholds": thresholds, "affine_fns": affine_fns}
 
     with open("analysis/approximations.json", "w") as f:
         json.dump(data, f)
