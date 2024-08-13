86c9c1d9b9cd421a1ddaabe7bc2f6b1b5d3d06c5
Mikolaj Deja <dejamikolaj@gmail.com>
2024-08-13 11:26:28 +0100

    Fix helper

--- helper.py
+++ helper.py
@@ -105,8 +105,7 @@ def get_linearly_probed(a, b, config, ps):
 def get_nn_sim(a, b, model_config, model_path, config):
     transform = transforms.Compose(
         [
-            transforms.Resize((256, 256)),
-            transforms.CenterCrop((224, 224)),
+            transforms.Resize((400, 400)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
