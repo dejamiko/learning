a9cf0e721a967223727cbcefac6924e6d7280eb9
Mikolaj Deja <dejamikolaj@gmail.com>
2024-08-25 11:59:50 +0100

    Fix runner

--- threshold_runner.sh
+++ threshold_runner.sh
@@ -2,11 +2,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=md1823
-#SBATCH --output=out_variable_threshold_%j.out
+#SBATCH --output=out_threshold_%j.out
 
 
 source /vol/bitbucket/md1823/taskmaster/learning/venv/bin/activate
 
 export PYTHONUNBUFFERED=TRUE
 
-python3 -m al.variable_threshold_solver
+python3 -m optim.threshold_approx_solver
